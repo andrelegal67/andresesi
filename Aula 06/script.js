121bc6d11a222c44f973f1cfa9c3f107a2faa56d
Fernanda_Maker03 <fernandanicheleint@gmail.com>
2026-05-26 09:15:09 -0300

    aula 06

---
+++ Aula 06/script.js
@@ -0,0 +1,33 @@
+/*
+var
+let 
+const
+*/
+
+
+let frutas = ["maçã", "uva", "pera"];
+
+console.log(frutas.length)
+
+
+
+let alunos = [
+    {
+        id:1,
+        nome: "Murilo",
+        sobrenome :"Chaves"
+    },
+    {
+        id:2,
+         nome: "Fernanda",
+         sobrenome :"Nichele"
+    }
+]
+ 
+function soma (n1, n2){
+    return n1 + n2
+}
+
+console.log(soma(10,20))
+console.log(soma(50,30))
+
